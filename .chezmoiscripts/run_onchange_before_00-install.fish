284b3c5596192775e025594e42ef18a102cd9dd1
Akshat Pradhan <akshatpradhan@users.noreply.github.com>
2024-01-09 16:06:56 -0600

    test

--- .chezmoiscripts/run_onchange_before_00-install.fish
+++ .chezmoiscripts/run_onchange_before_00-install.fish
@@ -1,79 +1,30 @@
 #!/usr/bin/env fish
 eval "$(/opt/homebrew/bin/brew shellenv)"
-# brew bundle --no-lock --file=~/Brewfile
+echo '
+{{ range .core.taps -}}
+tap {{ . | quote }}
+{{ end -}}
+{{ range .core.brews -}}
+brew {{ . | quote }}
+{{ end -}}
+{{ range .core.casks -}}
+cask {{ . | quote }}
+{{ end -}}
+{{ range .core.vscodes -}}
+vscode {{ . | quote }}
+{{ end -}}
 
-{{ $packages := list
-     "core"
-     "ide"
-     "cpp" -}}
+{{ range .ide.taps -}}
+tap {{ . | quote }}
+{{ end -}}
+{{ range .ide.brews -}}
+brew {{ . | quote }}
+{{ end -}}
+{{ range .ide.casks -}}
+cask {{ . | quote }}
+{{ end -}}
+{{ range .ide.vscodes -}}
+vscode {{ . | quote }}
+{{ end -}}
 
-{{ range $package := $packages }}
-{{ with index . $package }}
-    {{ range .taps -}}
-    echo "tap {{ . | quote }}"
-    {{ end -}}
-    {{ range .brews -}}
-    echo "brew {{ . | quote }}"
-    {{ end -}}
-    {{ range .casks -}}
-    echo "cask {{ . | quote }}"
-    {{ end -}}
-    {{ range .vscodes -}}
-    echo "vscode {{ . | quote }}"
-    {{ end -}}"
-{{ end }}
-echo "{{ $package }}" | brew bundle --no-lock --file=/dev/stdin
-{{ end }}
-
-# {{ $packages := list
-#      "core"
-#      "ide"
-#      "cpp" -}}
-
-# {{ range $package := $packages }}
-#     echo "{{ range .$package.taps -}}
-#     tap {{ . | quote }}
-#     {{ end -}}
-#     {{ range .$package.brews -}}
-#     brew {{ . | quote }}
-#     {{ end -}}
-#     {{ range .$package.casks -}}
-#     cask {{ . | quote }}
-#     {{ end -}}
-#     {{ range .$package.vscodes -}}
-#     vscode {{ . | quote }}
-#     {{ end -}}" | brew bundle --no-lock --file=/dev/stdin
-# {{ end }}
-
-
-
-
-# {{ range .ide.casks -}}
-# cask {{ . | quote }}
-# {{ end -}}
-# {{ range .ide.vscodes -}}
-# vscode {{ . | quote }}
-# {{ end -}}
-
-# {{ range .cpp.brews -}}
-# brews {{ . | quote }}
-# {{ end -}}
-# {{ range .cpp.brews -}}
-# vscode {{ . | quote }}
-# {{ end -}}
-
-
-
-# {{ range .utils.brews -}}
-# brew {{ . | quote }}
-# {{ end -}}
-# {{ range .utils.casks -}}
-# cask {{ . | quote }}
-# {{ end -}}
-# {{ range .utils.mas -}}
-
-
-
-
-
-# ' | brew bundle --no-lock --file=/dev/stdin
+' | brew bundle --no-lock --file=/dev/stdin
