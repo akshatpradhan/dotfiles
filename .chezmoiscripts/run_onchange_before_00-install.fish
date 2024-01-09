0b316b23d246cffac67bc73b1bdf16f393e043fa
Akshat Pradhan <akshatpradhan@users.noreply.github.com>
2024-01-09 04:22:06 -0600

    test

--- .chezmoiscripts/run_onchange_before_00-install.fish
+++ .chezmoiscripts/run_onchange_before_00-install.fish
@@ -4,18 +4,23 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 set packages core ide cpp
 
 for package in $packages
-    echo "{{ range .$package.taps -}}
-    tap {{ . | quote }}
-    {{ end -}}
-    {{ range .$package.brews -}}
-    brew {{ . | quote }}
-    {{ end -}}
-    {{ range .$package.casks -}}
-    cask {{ . | quote }}
-    {{ end -}}
-    {{ range .$package.vscodes -}}
-    vscode {{ . | quote }}
-    {{ end -}}" | brew bundle --no-lock --file=/dev/stdin
+    echo "
+    \{\{ range .$package.taps \-\}\}
+    tap \{\{ \. | quote \}\}
+    \{\{ end \-\}\}
+
+    \{\{ range .$package.brews \-\}\}
+    brew \{\{ \. | quote \}\}
+    \{\{ end \-\}\}
+
+    \{\{ range .$package.casks \-\}\}
+    cask {{ \. | quote }}
+    \{\{ end \-\}\}
+
+    \{\{ range .$package.vscodes \-\}\}
+    vscode \{\{ \. | quote \}\}
+    \{\{ end \-\}\}
+    " | brew bundle --no-lock --file=/dev/stdin
 end
 
 
