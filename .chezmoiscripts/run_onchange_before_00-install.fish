4c68d9b89359ed450ad15b55db4297d882f49bcd
Akshat Pradhan <akshatpradhan@users.noreply.github.com>
2024-01-09 04:37:06 -0600

    test

--- .chezmoiscripts/run_onchange_before_00-install.fish
+++ .chezmoiscripts/run_onchange_before_00-install.fish
@@ -1,27 +1,49 @@
 #!/usr/bin/env fish
 eval "$(/opt/homebrew/bin/brew shellenv)"
 # brew bundle --no-lock --file=~/Brewfile
-set packages core ide cpp
 
-for package in $packages
-    echo "
-    \{\{ range .$package.taps \-\}\}
-    tap \{\{ \. | quote \}\}
-    \{\{ end \-\}\}
+{{ $packages := list
+     "core"
+     "ide"
+     "cpp" -}}
 
-    \{\{ range .$package.brews \-\}\}
-    brew \{\{ \. | quote \}\}
-    \{\{ end \-\}\}
+{{ range $package := $packages }}
+{{ with index . $package }}
+    {{ range .taps -}}
+    echo "tap {{ . | quote }}"
+    {{ end -}}
+    {{ range .brews -}}
+    echo "brew {{ . | quote }}"
+    {{ end -}}
+    {{ range .casks -}}
+    echo "cask {{ . | quote }}"
+    {{ end -}}
+    {{ range .vscodes -}}
+    echo "vscode {{ . | quote }}"
+    {{ end -}}"
+{{ end }}
+echo "{{ $package }}" | brew bundle --no-lock --file=/dev/stdin
+{{ end }}
 
-    \{\{ range .$package.casks \-\}\}
-    cask {{ \. | quote }}
-    \{\{ end \-\}\}
+# {{ $packages := list
+#      "core"
+#      "ide"
+#      "cpp" -}}
 
-    \{\{ range .$package.vscodes \-\}\}
-    vscode \{\{ \. | quote \}\}
-    \{\{ end \-\}\}
-    " | brew bundle --no-lock --file=/dev/stdin
-end
+# {{ range $package := $packages }}
+#     echo "{{ range .$package.taps -}}
+#     tap {{ . | quote }}
+#     {{ end -}}
+#     {{ range .$package.brews -}}
+#     brew {{ . | quote }}
+#     {{ end -}}
+#     {{ range .$package.casks -}}
+#     cask {{ . | quote }}
+#     {{ end -}}
+#     {{ range .$package.vscodes -}}
+#     vscode {{ . | quote }}
+#     {{ end -}}" | brew bundle --no-lock --file=/dev/stdin
+# {{ end }}
 
 
 
