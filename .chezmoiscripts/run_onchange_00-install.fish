29cc2bde4c03a745e9814c9beb4960e7cb4f0b81
Akshat Pradhan <akshatpradhan@users.noreply.github.com>
2024-01-09 16:28:57 -0600

    test

--- .chezmoiscripts/run_onchange_00-install.fish
+++ .chezmoiscripts/run_onchange_00-install.fish
@@ -1,8 +1,9 @@
 #!/usr/bin/env fish
-eval "$(/opt/homebrew/bin/brew shellenv)"
+eval (/opt/homebrew/bin/brew shellenv)
+
 echo '{{ range .packages.brews -}}
 brew {{ . | quote }}
 {{ end -}}
 {{ range .packages.casks -}}
 cask {{ . | quote }}
-{{ end -}}'| brew bundle --no-lock --file=/dev/stdin
+{{ end -}}' | brew bundle --no-lock --file=/dev/stdin
