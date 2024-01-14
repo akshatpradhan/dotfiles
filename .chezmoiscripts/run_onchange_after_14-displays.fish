a6bf40d149da88dc0cc635fe34ad6bd9f83f2356
Akshat Pradhan <akshatpradhan@users.noreply.github.com>
2024-01-13 22:51:26 -0600

    test

--- .chezmoiscripts/run_onchange_after_14-displays.fish
+++ .chezmoiscripts/run_onchange_after_14-displays.fish
@@ -6,24 +6,24 @@
 # - [More Warm] Color Temperature (defualt: medium warm)
 
 # Not Working
-set CORE_BRIGHTNESS="/var/root/Library/Preferences/com.apple.CoreBrightness.plist"
+# set CORE_BRIGHTNESS="/var/root/Library/Preferences/com.apple.CoreBrightness.plist"
 
-set ENABLE='{
-  CBBlueReductionStatus =     {
-    AutoBlueReductionEnabled = 1;
-    BlueLightReductionDisableScheduleAlertCounter = 3;
-    BlueLightReductionSchedule =         {
-      DayStartHour = 7;
-      DayStartMinute = 0;
-      NightStartHour = 22;
-      NightStartMinute = 0;
-    };
-    BlueReductionEnabled = 0;
-    BlueReductionMode = 1;
-    BlueReductionSunScheduleAllowed = 1;
-    Version = 1;
-  };
-}'
+# set ENABLE='{
+#   CBBlueReductionStatus =     {
+#     AutoBlueReductionEnabled = 1;
+#     BlueLightReductionDisableScheduleAlertCounter = 3;
+#     BlueLightReductionSchedule =         {
+#       DayStartHour = 7;
+#       DayStartMinute = 0;
+#       NightStartHour = 22;
+#       NightStartMinute = 0;
+#     };
+#     BlueReductionEnabled = 0;
+#     BlueReductionMode = 1;
+#     BlueReductionSunScheduleAllowed = 1;
+#     Version = 1;
+#   };
+# }'
 
-sudo defaults write $CORE_BRIGHTNESS "CBUser-0" "$ENABLE"
-sudo defaults write $CORE_BRIGHTNESS "CBUser-$(dscl . -read ~ GeneratedUID | sed 's/GeneratedUID: //')" "$ENABLE"
+# sudo defaults write $CORE_BRIGHTNESS "CBUser-0" "$ENABLE"
+# sudo defaults write $CORE_BRIGHTNESS "CBUser-$(dscl . -read ~ GeneratedUID | sed 's/GeneratedUID: //')" "$ENABLE"
