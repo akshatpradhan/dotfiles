479cedd8915e6b5b5eb76130eaf05575418576ad
Akshat Pradhan <akshatpradhan@users.noreply.github.com>
2024-01-14 02:28:38 -0600

    test

--- .chezmoiscripts/run_onchange_after_24-keyboard.fish
+++ .chezmoiscripts/run_onchange_after_24-keyboard.fish
@@ -30,8 +30,8 @@ defaults write -g InitialKeyRepeat -int 15 # Working
 # </dict>'
 
 
-/usr/libexec/PlistBuddy -c "Set ':com.apple.keyboard.modifiermapping.0-0-0:0:HIDKeyboardModifierMappingDst' integer '30064771129'" "$HOME/Library/Preferences/ByHost/.GlobalPreferences.C598B614-0C56-5490-9359-F1455275B926.plist"
-/usr/libexec/PlistBuddy -c "Set ':com.apple.keyboard.modifiermapping.0-0-0:1:HIDKeyboardModifierMappingDst' integer '30064771129'" "$HOME/Library/Preferences/ByHost/.GlobalPreferences.C598B614-0C56-5490-9359-F1455275B926.plist"
-/usr/libexec/PlistBuddy -c "Set ':com.apple.keyboard.modifiermapping.0-0-0:1:HIDKeyboardModifierMappingSrc' integer '30064771129'" "$HOME/Library/Preferences/ByHost/.GlobalPreferences.C598B614-0C56-5490-9359-F1455275B926.plist"
-/usr/libexec/PlistBuddy -c "Set ':com.apple.keyboard.modifiermapping.0-0-0:2:HIDKeyboardModifierMappingSrc' integer '30064771296'" "$HOME/Library/Preferences/ByHost/.GlobalPreferences.C598B614-0C56-5490-9359-F1455275B926.plist"
-/usr/libexec/PlistBuddy -c "Set ':com.apple.keyboard.modifiermapping.0-0-0:1:HIDKeyboardModifierMappingDst' integer '30064771300'" "$HOME/Library/Preferences/ByHost/.GlobalPreferences.C598B614-0C56-5490-9359-F1455275B926.plist"
+/usr/libexec/PlistBuddy -c "Add ':com.apple.keyboard.modifiermapping.0-0-0:0:HIDKeyboardModifierMappingDst' integer '30064771129'" "$HOME/Library/Preferences/ByHost/.GlobalPreferences.C598B614-0C56-5490-9359-F1455275B926.plist"
+/usr/libexec/PlistBuddy -c "Add ':com.apple.keyboard.modifiermapping.0-0-0:1:HIDKeyboardModifierMappingDst' integer '30064771129'" "$HOME/Library/Preferences/ByHost/.GlobalPreferences.C598B614-0C56-5490-9359-F1455275B926.plist"
+/usr/libexec/PlistBuddy -c "Add ':com.apple.keyboard.modifiermapping.0-0-0:1:HIDKeyboardModifierMappingSrc' integer '30064771129'" "$HOME/Library/Preferences/ByHost/.GlobalPreferences.C598B614-0C56-5490-9359-F1455275B926.plist"
+/usr/libexec/PlistBuddy -c "Add ':com.apple.keyboard.modifiermapping.0-0-0:2:HIDKeyboardModifierMappingSrc' integer '30064771296'" "$HOME/Library/Preferences/ByHost/.GlobalPreferences.C598B614-0C56-5490-9359-F1455275B926.plist"
+/usr/libexec/PlistBuddy -c "Add ':com.apple.keyboard.modifiermapping.0-0-0:1:HIDKeyboardModifierMappingDst' integer '30064771300'" "$HOME/Library/Preferences/ByHost/.GlobalPreferences.C598B614-0C56-5490-9359-F1455275B926.plist"
