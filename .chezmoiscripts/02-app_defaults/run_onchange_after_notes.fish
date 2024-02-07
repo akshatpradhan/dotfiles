2c76a4d0a94356a157dfe933f739b472e53e1613
AP <akshatpradhan@users.noreply.github.com>
2024-02-06 18:40:23 -0600

    notes

--- .chezmoiscripts/02-app_defaults/run_onchange_after_notes.fish
+++ .chezmoiscripts/02-app_defaults/run_onchange_after_notes.fish
@@ -9,7 +9,7 @@
 # =======
 # Left hand side             Right Hand Side
 # Flexible Space | Delete    New Note | Flexible Space |  Format | Checklist | Table | Link | Flexible Space | Lock | Share | Search
-set plist ~/Library/Containers/com.apple.Notes/Data/Library/Preferences/com.apple.Notes.plist
+set plist $HOME/Library/Containers/com.apple.Notes/Data/Library/Preferences/com.apple.Notes.plist
 # /usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers'" $plist 2>/dev/null
 # /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers' array" $plist
 # set -l toolbar_items \
@@ -34,20 +34,21 @@ set plist ~/Library/Containers/com.apple.Notes/Data/Library/Preferences/com.appl
 
 # /usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers'" $plist
 /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers' array" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:' string 'FoldersToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:' string 'NSToolbarSidebarTrackingSeparatorItemIdentifier'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:' string 'NSToolbarFlexibleSpaceItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:' string 'NoteListEditorSeparatorToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:' string 'NewNoteToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:' string 'NSToolbarFlexibleSpaceItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:' string 'FormatToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:' string 'ChecklistToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:' string 'TableToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:' string 'LinkToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:' string 'NSToolbarFlexibleSpaceItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:' string 'LockToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:' string 'CollaborationToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:' string 'SearchToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:0' string 'FoldersToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:1' string 'NSToolbarSidebarTrackingSeparatorItemIdentifier'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:2' string 'NSToolbarFlexibleSpaceItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:3' string 'DeleteToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:4' string 'NoteListEditorSeparatorToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:5' string 'NewNoteToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:6' string 'NSToolbarFlexibleSpaceItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:7' string 'FormatToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:8' string 'ChecklistToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:9' string 'TableToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:10' string 'LinkToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:11' string 'NSToolbarFlexibleSpaceItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:12' string 'LockToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:13' string 'CollaborationToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:14' string 'SearchToolbarItem'" $plist
 
 
 
