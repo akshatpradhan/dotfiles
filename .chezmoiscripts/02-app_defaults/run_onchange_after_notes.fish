8311ba22251dcaee8c117e3663b8de539f21d0a0
AP <akshatpradhan@users.noreply.github.com>
2024-02-06 20:41:48 -0600

    notes

--- .chezmoiscripts/02-app_defaults/run_onchange_after_notes.fish
+++ .chezmoiscripts/02-app_defaults/run_onchange_after_notes.fish
@@ -10,7 +10,7 @@
 # Left hand side             Right Hand Side
 # Flexible Space | Delete    New Note | Flexible Space | Format | Checklist | Table | Link | Flexible Space | Lock | Share | Search
 set plist $HOME/Library/Containers/com.apple.Notes/Data/Library/Preferences/com.apple.Notes.plist
-/usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers'" $plist
+/usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers'" $plist  &> /dev/null
 /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers' array" $plist
 /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:0' string 'FoldersToolbarItem'" $plist
 /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:1' string 'NSToolbarSidebarTrackingSeparatorItemIdentifier'" $plist
@@ -33,7 +33,7 @@ set plist $HOME/Library/Containers/com.apple.Notes/Data/Library/Preferences/com.
 # Toolbar for Single Note Window
 # ==============================
 # Delete | Space | Format | Checklist | Table | Link | Space | Lock | Share 
-/usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers'" $plist
+/usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers'" $plist  &> /dev/null
 /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers' array" $plist
 /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:0' string 'DeleteToolbarItem'" $plist
 /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:1' string 'NSToolbarSpaceItem'" $plist
