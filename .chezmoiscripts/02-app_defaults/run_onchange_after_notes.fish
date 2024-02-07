0a4fb1a6257e542da7e1a3107319372137e9a76b
AP <akshatpradhan@users.noreply.github.com>
2024-02-06 19:20:53 -0600

    notes

--- .chezmoiscripts/02-app_defaults/run_onchange_after_notes.fish
+++ .chezmoiscripts/02-app_defaults/run_onchange_after_notes.fish
@@ -4,35 +4,12 @@
 # Notes -> Settings
 # [iCloud] Default account
 
-# =======
-# Toolbar
-# =======
+# =======================
+# Toolbar for Main Window
+# =======================
 # Left hand side             Right Hand Side
-# Flexible Space | Delete    New Note | Flexible Space |  Format | Checklist | Table | Link | Flexible Space | Lock | Share | Search
+# Flexible Space | Delete    New Note | Flexible Space | Format | Checklist | Table | Link | Flexible Space | Lock | Share | Search
 set plist $HOME/Library/Containers/com.apple.Notes/Data/Library/Preferences/com.apple.Notes.plist
-# /usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers'" $plist 2>/dev/null
-# /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers' array" $plist
-# set -l toolbar_items \
-#   FoldersToolbarItem \
-#   NSToolbarSidebarTrackingSeparatorItemIdentifier \
-#   NSToolbarFlexibleSpaceItem \
-#   NoteListEditorSeparatorToolbarItem \
-#   NewNoteToolbarItem \
-#   NSToolbarFlexibleSpaceItem \
-#   FormatToolbarItem \
-#   ChecklistToolbarItem \
-#   TableToolbarItem \
-#   LinkToolbarItem \
-#   NSToolbarFlexibleSpaceItem \
-#   LockToolbarItem \
-#   CollaborationToolbarItem \
-#   SearchToolbarItem
-# for item in $toolbar_items
-#   /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:' string '$item'" $plist
-# end  
-
-
-
 /usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers'" $plist
 /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers' array" $plist
 /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:0' string 'FoldersToolbarItem'" $plist
@@ -52,7 +29,18 @@ set plist $HOME/Library/Containers/com.apple.Notes/Data/Library/Preferences/com.
 /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:14' string 'SearchToolbarItem'" $plist
 
 
-
-# Quick Notes
-# View -> Customize Toolbar
-# Delete | Space | Format | Checklist | Table | Space | Link | Media | Lock
+# ==============================
+# Toolbar for Single Note Window
+# ==============================
+# Delete | Space | Format | Checklist | Table | Link | Space | Lock | Share 
+/usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers' array" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:0' string 'DeleteToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:1' string 'NSToolbarSpaceItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:2' string 'FormatToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:3' string 'ChecklistToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:4' string 'TableToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:5' string 'LinkToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:6' string 'NSToolbarSpaceItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:7' string 'LockToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:8' string 'CollaborationToolbarItem'" $plist
