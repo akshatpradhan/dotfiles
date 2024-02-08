c099cce89e7af2e7bba8819a8f6309fc92b7a9d5
AP <akshatpradhan@users.noreply.github.com>
2024-02-07 19:48:23 -0600

    preview

--- .chezmoiscripts/02-app_defaults/run_onchange_after_preview.fish
+++ .chezmoiscripts/02-app_defaults/run_onchange_after_preview.fish
@@ -1,22 +1,25 @@
 #!/usr/bin/env fish
+set plist $HOME/Library/Containers/com.apple.Preview/Data/Library/Preferences/com.apple.Preview.plist
 # ======
 # Images
 # ======
 # [Open all files in one window] When opening files (default: Open groups of files in the same window)
-/usr/libexec/PlistBuddy -c "Delete ':PVImageOpeningMode'" -c "Add ':PVImageOpeningMode' integer '0'" "$HOME/Library/Containers/com.apple.Preview/Data/Library/Preferences/com.apple.Preview.plist"
+defaults write com.apple.Preview "PVImageOpeningMode" -int 0
+# /usr/libexec/PlistBuddy -c "Delete ':PVImageOpeningMode'" -c "Add ':PVImageOpeningMode' integer '0'" "$HOME/Library/Containers/com.apple.Preview/Data/Library/Preferences/com.apple.Preview.plist"
 
 # [ ] Add name to annotations (default: on)
-/usr/libexec/PlistBuddy -c "Delete ':PVGeneralUseUserName'" -c "Add ':PVGeneralUseUserName' bool 'false'" "$HOME/Library/Containers/com.apple.Preview/Data/Library/Preferences/com.apple.Preview.plist"
+defaults write com.apple.Preview "PVGeneralUseUserName" -bool false
+# /usr/libexec/PlistBuddy -c "Delete ':PVGeneralUseUserName'" -c "Add ':PVGeneralUseUserName' bool 'false'" "$HOME/Library/Containers/com.apple.Preview/Data/Library/Preferences/com.apple.Preview.plist"
 
 
 # View -> Show Tab Bar
-/usr/libexec/PlistBuddy -c "Delete ':NSWindowTabbingShoudShowTabBarKey-PVWindow-PVWindowController-PVWindowController-VT-FS'" -c "Add ':NSWindowTabbingShoudShowTabBarKey-PVWindow-PVWindowController-PVWindowController-VT-FS' bool 'true'" "$HOME/Library/Containers/com.apple.Preview/Data/Library/Preferences/com.apple.Preview.plist"
+defaults write com.apple.Preview "NSWindowTabbingShoudShowTabBarKey-PVWindow-PVWindowController-PVWindowController-VT-FS" -bool true
+# /usr/libexec/PlistBuddy -c "Delete ':NSWindowTabbingShoudShowTabBarKey-PVWindow-PVWindowController-PVWindowController-VT-FS'" -c "Add ':NSWindowTabbingShoudShowTabBarKey-PVWindow-PVWindowController-PVWindowController-VT-FS' bool 'true'" "$HOME/Library/Containers/com.apple.Preview/Data/Library/Preferences/com.apple.Preview.plist"
 
 # =======================
 # Toolbar for Main Window
 # =======================
 # Markup | Highlight | Form Filling | Actual Size | Share | Page | Search
-set plist $HOME/Library/Containers/com.apple.Preview/Data/Library/Preferences/com.apple.Preview.plist
 /usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers'" $plist
 /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers' array" $plist
 /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:0' string 'view'" $plist
