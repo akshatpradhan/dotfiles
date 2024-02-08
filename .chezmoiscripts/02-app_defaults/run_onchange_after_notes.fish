a179d27bdcc6ff7b33e5ac6d94e26119deca3807
AP <akshatpradhan@users.noreply.github.com>
2024-02-07 21:16:44 -0600

    notes

--- .chezmoiscripts/02-app_defaults/run_onchange_after_notes.fish
+++ .chezmoiscripts/02-app_defaults/run_onchange_after_notes.fish
@@ -1,5 +1,7 @@
 #!/usr/bin/env fish
 
+set plist $HOME/Library/Containers/com.apple.Notes/Data/Library/Preferences/com.apple.Notes.plist
+
 # TODO: Configure using iCloud after configuring iCloud login
 # Notes -> Settings
 # [iCloud] Default account
@@ -9,38 +11,61 @@
 # =======================
 # Left hand side             Right Hand Side
 # Flexible Space | Delete    New Note | Flexible Space | Format | Checklist | Table | Link | Flexible Space | Lock | Share | Search
-set plist $HOME/Library/Containers/com.apple.Notes/Data/Library/Preferences/com.apple.Notes.plist
-/usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers'" $plist  &> /dev/null
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers' array" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:0' string 'FoldersToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:1' string 'NSToolbarSidebarTrackingSeparatorItemIdentifier'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:2' string 'NSToolbarFlexibleSpaceItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:3' string 'DeleteToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:4' string 'NoteListEditorSeparatorToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:5' string 'NewNoteToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:6' string 'NSToolbarFlexibleSpaceItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:7' string 'FormatToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:8' string 'ChecklistToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:9' string 'TableToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:10' string 'LinkToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:11' string 'NSToolbarFlexibleSpaceItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:12' string 'LockToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:13' string 'CollaborationToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:14' string 'SearchToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Delete ':NSToolbar Configuration MainWindowToolbar:TB Item Identifiers'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Item Identifiers' array" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:0' string 'FoldersToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:1' string 'NSToolbarSidebarTrackingSeparatorItemIdentifier'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:2' string 'NSToolbarFlexibleSpaceItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:3' string 'DeleteToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:4' string 'NoteListEditorSeparatorToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:5' string 'NewNoteToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:6' string 'NSToolbarFlexibleSpaceItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:7' string 'FormatToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:8' string 'ChecklistToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:9' string 'TableToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:10' string 'LinkToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:11' string 'NSToolbarFlexibleSpaceItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:12' string 'LockToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:13' string 'CollaborationToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Item Identifiers:14' string 'SearchToolbarItem'" $plist
+# [Icon and Text] Show (default: Icon Only)
+/usr/libexec/PlistBuddy -c "Delete ':NSToolbar Configuration MainWindowToolbar:TB Display Mode'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Display Mode' integer 1" $plist
+# =======================
+# Boilerplate Necesstiies
+# =======================
+/usr/libexec/PlistBuddy -c "Delete ':NSToolbar Configuration MainWindowToolbar:TB Icon Size Mode'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Icon Size Mode' integer 1" $plist
+/usr/libexec/PlistBuddy -c "Delete ':NSToolbar Configuration MainWindowToolbar:TB Is Shown'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB Is Shown' bool true" $plist
+/usr/libexec/PlistBuddy -c "Delete ':NSToolbar Configuration MainWindowToolbar:TB SizeMode'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration MainWindowToolbar:TB SizeMode' integer 1" $plist
 
 
 # ==============================
 # Toolbar for Single Note Window
 # ==============================
 # Delete | Space | Format | Checklist | Table | Link | Space | Lock | Share 
-/usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers'" $plist  &> /dev/null
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers' array" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:0' string 'DeleteToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:1' string 'NSToolbarSpaceItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:2' string 'FormatToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:3' string 'ChecklistToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:4' string 'TableToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:5' string 'LinkToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:6' string 'NSToolbarSpaceItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:7' string 'LockToolbarItem'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:8' string 'CollaborationToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Delete ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers'" $plist  &> /dev/null
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers' array" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:0' string 'DeleteToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:1' string 'NSToolbarSpaceItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:2' string 'FormatToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:3' string 'ChecklistToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:4' string 'TableToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:5' string 'LinkToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:6' string 'NSToolbarSpaceItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:7' string 'LockToolbarItem'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Item Identifiers:8' string 'CollaborationToolbarItem'" $plist
+# [Icon and Text] Show (default: Icon Only)
+/usr/libexec/PlistBuddy -c "Delete ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Display Mode'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Display Mode' integer 1" $plist
+# =======================
+# Boilerplate Necesstiies
+# =======================
+/usr/libexec/PlistBuddy -c "Delete ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Icon Size Mode'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Icon Size Mode' integer 1" $plist
+/usr/libexec/PlistBuddy -c "Delete ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Is Shown'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB Is Shown' bool true" $plist
+/usr/libexec/PlistBuddy -c "Delete ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB SizeMode'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration ICMSingleNoteWindowToolbarIdentifier:TB SizeMode' integer 1" $plist
