21f862bac1442378f643d5d2bf76dd3a31544f87
AP <akshatpradhan@users.noreply.github.com>
2024-02-07 18:48:43 -0600

    preview

--- .chezmoiscripts/02-app_defaults/run_onchange_after_preview.fish
+++ .chezmoiscripts/02-app_defaults/run_onchange_after_preview.fish
@@ -3,21 +3,21 @@
 # Images
 # ======
 # [Open all files in one window] When opening files (default: Open groups of files in the same window)
-/usr/libexec/PlistBuddy -c "Delete ':PVImageOpeningMode'" -c "Add ':PVImageOpeningMode' integer '0'" "$HOME/Library/Containers/com.apple.Preview/Data/Library/Preferences/com.apple.Preview.plist"
+# /usr/libexec/PlistBuddy -c "Delete ':PVImageOpeningMode'" -c "Add ':PVImageOpeningMode' integer '0'" "$HOME/Library/Containers/com.apple.Preview/Data/Library/Preferences/com.apple.Preview.plist"
 
 # [ ] Add name to annotations (default: on)
-/usr/libexec/PlistBuddy -c "Delete ':PVGeneralUseUserName'" -c "Add ':PVGeneralUseUserName' bool 'false'" "$HOME/Library/Containers/com.apple.Preview/Data/Library/Preferences/com.apple.Preview.plist"
+# /usr/libexec/PlistBuddy -c "Delete ':PVGeneralUseUserName'" -c "Add ':PVGeneralUseUserName' bool 'false'" "$HOME/Library/Containers/com.apple.Preview/Data/Library/Preferences/com.apple.Preview.plist"
 
 
 # View -> Show Tab Bar
-/usr/libexec/PlistBuddy -c "Delete ':NSWindowTabbingShoudShowTabBarKey-PVWindow-PVWindowController-PVWindowController-VT-FS'" -c "Add ':NSWindowTabbingShoudShowTabBarKey-PVWindow-PVWindowController-PVWindowController-VT-FS' bool 'true'" "$HOME/Library/Containers/com.apple.Preview/Data/Library/Preferences/com.apple.Preview.plist"
+# /usr/libexec/PlistBuddy -c "Delete ':NSWindowTabbingShoudShowTabBarKey-PVWindow-PVWindowController-PVWindowController-VT-FS'" -c "Add ':NSWindowTabbingShoudShowTabBarKey-PVWindow-PVWindowController-PVWindowController-VT-FS' bool 'true'" "$HOME/Library/Containers/com.apple.Preview/Data/Library/Preferences/com.apple.Preview.plist"
 
 # =======================
 # Toolbar for Main Window
 # =======================
 # Markup | Highlight | Form Filling | Actual Size | Share | Page | Search
 set plist $HOME/Library/Containers/com.apple.Preview/Data/Library/Preferences/com.apple.Preview.plist
-/usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers'" $plist  &> /dev/null
+/usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers'" $plist
 /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers' array" $plist
 /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:0' string 'view'" $plist
 /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:1' string 'edit_banner'" $plist
@@ -29,6 +29,6 @@ set plist $HOME/Library/Containers/com.apple.Preview/Data/Library/Preferences/co
 /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:7' string 'search'" $plist
 
 # [Icon and Text] Show
-/usr/libexec/PlistBuddy -c "Delete ':NSToolbar Configuration CommonToolbar_v5.1:TB Display Mode'" $plist
-/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration CommonToolbar_v5.1:TB Display Mode' integer '1'" $plist
+# /usr/libexec/PlistBuddy -c "Delete ':NSToolbar Configuration CommonToolbar_v5.1:TB Display Mode'" $plist
+# /usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration CommonToolbar_v5.1:TB Display Mode' integer '1'" $plist
 
