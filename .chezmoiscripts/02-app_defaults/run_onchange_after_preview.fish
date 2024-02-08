12a9790f29abe941687fa21b9fbfdc0383959f8c
AP <akshatpradhan@users.noreply.github.com>
2024-02-07 20:57:25 -0600

    preview

--- .chezmoiscripts/02-app_defaults/run_onchange_after_preview.fish
+++ .chezmoiscripts/02-app_defaults/run_onchange_after_preview.fish
@@ -20,20 +20,23 @@ defaults write com.apple.Preview "PVGeneralUseUserName" -bool false
 # Toolbar for Main Window
 # =======================
 # Markup | Highlight | Form Filling | Actual Size | Share | Page | Search
-/usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers' array" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:0' string 'view'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:1' string 'edit_banner'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:2' string 'markup'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:3' string 'form_filling'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:4' string 'actual_size'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:5' string 'share'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:6' string 'goto_page'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:7' string 'search'" $plist
+/usr/libexec/PlistBuddy -c "Delete ':NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers' array" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:0' string 'view'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:1' string 'edit_banner'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:2' string 'markup'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:3' string 'form_filling'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:4' string 'actual_size'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:5' string 'share'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:6' string 'goto_page'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration CommonToolbar_v5.1:TB Item Identifiers:7' string 'search'" $plist
 # [Icon and Text] Show (default: Icon Only)
-/usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration CommonToolbar_v5.1:TB Display Mode'" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Display Mode' integer 1" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Icon Size Mode' integer 1" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB Is Shown' boolean true" $plist
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration CommonToolbar_v5.1:TB SizeMode' integer 1" $plist
+/usr/libexec/PlistBuddy -c "Delete ':NSToolbar Configuration CommonToolbar_v5.1:TB Display Mode'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration CommonToolbar_v5.1:TB Display Mode' integer 1" $plist
+/usr/libexec/PlistBuddy -c "Delete ':NSToolbar Configuration CommonToolbar_v5.1:TB Icon Size Mode'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration CommonToolbar_v5.1:TB Icon Size Mode' integer 1" $plist
+/usr/libexec/PlistBuddy -c "Delete ':NSToolbar Configuration CommonToolbar_v5.1:TB Is Shown'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration CommonToolbar_v5.1:TB Is Shown' bool true" $plist
+/usr/libexec/PlistBuddy -c "Delete ':NSToolbar Configuration CommonToolbar_v5.1:TB SizeMode'" $plist
+/usr/libexec/PlistBuddy -c "Add ':NSToolbar Configuration CommonToolbar_v5.1:TB SizeMode' integer 1" $plist
 
